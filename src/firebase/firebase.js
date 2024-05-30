7e1c91b322b36cb7449c86f2d27fe61e9b9986d1
Broken Developer <150446481+BrokenDev2007@users.noreply.github.com>
2024-05-30 10:37:39 +0530

    update your credentials here!

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA24SLdKfcZto3deVtIz82OF7pMYKkCFRI",
-  authDomain: "tweetfortweet-f16fc.firebaseapp.com",
-  databaseURL: "https://tweetfortweet-f16fc-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tweetfortweet-f16fc",
-  storageBucket: "tweetfortweet-f16fc.appspot.com",
-  messagingSenderId: "493259706372",
-  appId: "1:493259706372:web:751dacd78ac44312a6e551",
-  measurementId: "G-8HPBYBNBN9"
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR DOMAIN",
+  databaseURL: "YOUR DB URL",
+  projectId: "YOUR ID",
+  storageBucket: "YOUR BUCKET ID",
+  messagingSenderId: "YOUR SENDER ID",
+  appId: "YOUR APP ID",
+  measurementId: "YOUR MEASUREMENT ID"
 };
 
 // Initialize Firebase
